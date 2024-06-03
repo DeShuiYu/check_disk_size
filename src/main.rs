9b669ba6cd983f27e038672bfc09db25296523fd
dsy <dsy@MacBook-Pro-M1.local>
2024-06-03 15:47:15 +0800

    add:readme.md

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use lettre::transport::smtp::authentication::Credentials;
 use prettytable::{Cell, Row};
 use walkdir::WalkDir;
 
-fn get_size_with_dir(dir_path:&Path)->Result<u64,io::Error>{
+fn get_size_with_dir(dir_path: &Path) -> Result<u64, io::Error> {
     let mut all_size = 0u64;
-    for dir_entry in WalkDir::new(dir_path){
+    for dir_entry in WalkDir::new(dir_path) {
         match dir_entry {
-            Ok(entry)=>{
-                if entry.file_type().is_file(){
+            Ok(entry) => {
+                if entry.file_type().is_file() {
                     all_size += entry.metadata().unwrap().len();
                 }
-            },
-            Err(e)=>{
-                eprintln!("{:?}",e);
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
             }
         }
     }
@@ -26,12 +26,12 @@ fn get_size_with_dir(dir_path:&Path)->Result<u64,io::Error>{
 }
 
 #[allow(dead_code)]
-fn send_mail(from_mail:&str,to_mail:&str,content:String){
+fn send_mail(from_mail: &str, to_mail: &str, content: String) {
     let local_time = chrono::prelude::Local::now();
     let email = Message::builder()
         .from(from_mail.parse().unwrap())
         .to(to_mail.parse().unwrap())
-        .subject(format!("{:?} 检查磁盘情况报告",local_time.format("%Y-%m-%d")))
+        .subject(format!("{:?} 检查磁盘情况报告", local_time.format("%Y-%m-%d")))
         .body(content)
         .expect("failed to create message");
 
@@ -42,35 +42,35 @@ fn send_mail(from_mail:&str,to_mail:&str,content:String){
         .credentials(credentials)
         .build();
     // 发送邮件
-   mailer.send(&email).expect("Failed to send mail");
+    mailer.send(&email).expect("Failed to send mail");
 }
 
-#[derive(Parser,Debug)]
-struct Args{
-    #[arg(short='d',long="dir",required = true)]
-    dir_path:PathBuf,
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short = 'd', long = "dir", required = true)]
+    dir_path: PathBuf,
 }
 
-fn main(){
+fn main() {
     let args = Args::parse();
     let mut table = prettytable::Table::new();
     table.add_row(Row::new(vec![
         Cell::new("文件路径").style_spec("c"),
-        Cell::new("占用大小").style_spec("c")
+        Cell::new("占用大小").style_spec("c"),
     ]));
-    println!("args:{:?}",args);
-    for direntry in fs::read_dir(args.dir_path.as_path()).expect("read dir error"){
+    println!("args:{:?}", args);
+    for direntry in fs::read_dir(args.dir_path.as_path()).expect("read dir error") {
         match direntry {
-            Ok(entry)=>{
-                if let Ok(size) = get_size_with_dir(&entry.path()){
+            Ok(entry) => {
+                if let Ok(size) = get_size_with_dir(&entry.path()) {
                     table.add_row(Row::new(vec![
-                        Cell::new(&format!("{:?}",entry.path())).style_spec("l"),
-                        Cell::new(&humansize::format_size(size,WINDOWS)).style_spec("c")
+                        Cell::new(&format!("{:?}", entry.path())).style_spec("l"),
+                        Cell::new(&humansize::format_size(size, WINDOWS)).style_spec("c"),
                     ]));
                 }
-            },
-            Err(e)=>{
-                eprintln!("{:?}",e);
+            }
+            Err(e) => {
+                eprintln!("{:?}", e);
             }
         }
     }
